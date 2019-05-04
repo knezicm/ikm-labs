231d2211a7817899051c4609d7c00990171b33a8
Mladen Knezic <mladen.knezic@gmail.com>
2019-05-04 10:15:01 +0200

    Added skeleton code for modbus protocol

---
+++ lab5/modbus-master.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <modbus.h>
+
+#define SERVER_ID		1
+#define COIL_ADDRESS		0
+#define BCM_PIN_DE		17
+
+int main()
+{
+
+	modbus_t *ctx;
+
+	// Add your code here
+
+	return 0;
+}
